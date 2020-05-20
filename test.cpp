e318107cf81cb3d5dfccc32b9fd3ce635c216515
haka913 <ace95913@gmail.com>
2020-05-21 01:23:57 +0900

    test

--- test.cpp
+++ test.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
 int main(){
     int a;
     cin >> a;
     cout <<"hello test1"<<endl;
-    cout << a;
+    cout << a<<endl;
+    for(int i = 0 ; i <10;i++){
+        cout << i+1 << " ";
+    }
+    vector<int> v;
+    v.push_back(3);
+    cout << endl;
+    cout << v[0];
     return 0;
 }
